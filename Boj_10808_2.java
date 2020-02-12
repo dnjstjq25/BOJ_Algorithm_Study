4ed4927e9ecde9e44f224e87a4dba34a29a2cb44
dnjstjq25 <54284354+dnjstjq25@users.noreply.github.com>
2020-02-12 10:16:11 +0900

    10808_다른사람꺼

---
+++ Boj_10808_2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.next(); //문자열 입력받음
+		
+		int[] res = new int[26]; // 알파벳 자리 증가를 위한 배열
+		
+		for(int i=0; i<input.length(); i++) {
+			res[input.charAt(i)-'a']++; //입력받은 문자열 - 'a'를 연산한 결과값을 res의 자리로 지정하고 1씩 증가
+		}
+		
+		for (int i : res) {
+			System.out.print(i+" ");  // 출력
+		}
+		
+	}
+}
